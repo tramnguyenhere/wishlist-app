01551362a173a70b03c7fd49a8e0039bdc5a6007
Tram Nguyen <tramngoc1402@gmail.com>
2022-12-18 21:53:13 +0200

    Finished deployment

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const config = require('./utils/config');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 const wishlistRouter = require('./controllers/wishlist.js');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
@@ -20,10 +21,19 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('build'));
+
+// app.use(express.static('build'));
 app.use(middleware.requestLogger);
 
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, './build')));
+
 app.use('/api/wishlist', wishlistRouter);
+
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/build/index.html'));
+});
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
