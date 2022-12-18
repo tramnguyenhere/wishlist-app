52b4839ee2767016e9acd2b5070cd704cbda66ca
Tram Nguyen <tramngoc1402@gmail.com>
2022-12-18 21:01:34 +0200

    Working on deploying app to the internet

--- client/src/services/wishlist.js
+++ client/src/services/wishlist.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:5000/api/wishlist';
+const baseUrl = '/api/wishlist';
 
 const getAll = async () => {
   const response = await axios.get(baseUrl);
--- server/app.js
+++ server/app.js
@@ -20,6 +20,7 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('build'));
 app.use(middleware.requestLogger);
 
 app.use('/api/wishlist', wishlistRouter);
